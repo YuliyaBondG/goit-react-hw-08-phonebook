b554615ea0ed73936de5569791b20fea77098d26
YuliyaBondG <113913438+YuliyaBondG@users.noreply.github.com>
2023-06-11 20:40:23 +0000

    Deploying to gh-pages from @ YuliyaBondG/goit-react-hw-08-phonebook@3e4622b181ef3087a7a45d5b69ed429a0f01630c 🚀

---
+++ static/js/266.78ae757c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var r=o(5392),t=o(184);function c(){return(0,t.jsxs)(r.$0,{children:[(0,t.jsx)(r.s2,{children:"phone"}),(0,t.jsx)(r.so,{children:"book"}),(0,t.jsx)(r.Nu,{children:"Your own personal book of contacts"})]})}}}]);
+//# sourceMappingURL=266.78ae757c.chunk.js.map
